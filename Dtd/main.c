80753f176d588df1e9b1ebf932ed330c960e045d
David <nguyen.david.1909@gmail.com>
2020-10-22 00:02:44 +0200

    Edit File

--- Dtd/main.c
+++ Dtd/main.c
@@ -1,8 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main()
-{
-    printf("Hello world!\n");
+void openFile(FILE * f,char *name, char *code);
+
+int main(){
+
+    FILE *f;
+    char *name = "exemple.xml";
+    char *fileCode = "r+";
+
+    openFile(f, name, fileCode);
+
     return 0;
+}
+
+void openFile(FILE * f,char *name, char *code){
+
+    f = fopen(name, code);
+    char * result = malloc(sizeof(char) * (strlen(name)*10));
+
+    char *test = malloc(sizeof(char) * (strlen(name)*10));
+
+    if(f != NULL){
+
+        printf("Fichier ouvert\n");
+
+        while (fgets(result, strlen(name)*10, f), !feof(f)){
+
+            test = strchr(result, '<');
+            printf("%s", test);
+            //printf("%ld", ftell(f));
+
+        }
+
+    }else{
+
+        printf("Fichier non ouvert");
+
+    }
+
 }
