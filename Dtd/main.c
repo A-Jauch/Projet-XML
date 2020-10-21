dd6a2f0731fed2a079f37ea6da9e280ea9961de3
antho <jauch-anthony@hotmail.fr>
2020-10-21 14:55:54 -0700

    open xml

--- Dtd/main.c
+++ Dtd/main.c
@@ -1,8 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main()
+
+
+void openFile (FILE * f,char* name, char* code);
+
+void openFile (FILE * f,char* name, char* code){
+
+    f = fopen(name, code);
+
+    if( f != NULL ){
+
+        printf("Fichier ouvert\n");
+        fclose(f);
+
+    } else {
+        printf("Fichier non ouvert");
+        fclose(f);
+
+    }
+
+
+}
+
+
+
+int main(int argc, char * argv[])
 {
-    printf("Hello world!\n");
+    FILE * fp;
+    char * fileName = "exemple.xml";
+    char * fileCode = "r";
+
+    openFile(fp, fileName , fileCode );
     return 0;
 }
